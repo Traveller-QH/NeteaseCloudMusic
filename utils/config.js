ec415db90eaad8633fc4bc3563bf445e612f785a
Traveller_QH <2967812410@qq.com>
2026-03-01 23:53:41 +0800

    feat:新增线上环境

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,10 @@
 // API基础配置
 const config = {
 	// 后端API地址
-	baseUrl: 'http://localhost:3000',
+	// 本地环境
+	// baseUrl: 'http://localhost:3000',
+	// 线上环境
+	baseUrl: 'https://miraitowa.cloud/netease-cloud-music-api/',
 	
 	// 请求超时时间
 	timeout: 15000,
